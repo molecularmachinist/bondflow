99ac338e0f7200cf7b34385d5f14dd306d107bdf
micaelamaria <129414127+micaelamaria@users.noreply.github.com>
2024-05-29 14:37:33 +0300

    Update ma_sb_analysis.py

--- salt_bridge_analysis/ma_sb_analysis.py
+++ salt_bridge_analysis/ma_sb_analysis.py
@@ -13,7 +13,10 @@ ma_binary_contact_map = ba.moving_average_contact_map_maker(contact_map_names, b
 
 distance_df, sb_names_list = ba.create_sorted_distance_dataframe(contact_map_names, ma_binary_contact_map, distance_map)
 
+distance_df.to_csv('ma_distance_data.csv', index=False)
+
 ma_contact_map, ma_contact_map_names = ba.create_contact_map_from_dataframe(distance_df)
+
 
 with open('ma_contact_map.npy', 'wb') as f:
 
