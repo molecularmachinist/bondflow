253e0bf7f74f1f2e0ad00045c0e8f40cb15e4709
micaelamaria <129414127+micaelamaria@users.noreply.github.com>
2024-05-29 13:52:22 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,26 @@
-import find_bond_functions
-import sys
+import bond_analysis as ba
+import numpy as np
 
-reference_struc = sys.argv[1]
-trajectory = sys.argv[2]
 
-contact_map, contact_map_names = find_bond_functions.salt_bridge_contact_map(reference_struc, trajectory)
-                            
-                            
-i = find_bond_functions.std_contact(contact_map)
+reference_struc = 'centered_active.gro' 
+trajectory = 'reduced_frames.xtc' 
 
-df = find_bond_functions.sb_time_dependence(contact_map, contact_map_names, i)
+print('Making contact maps, this will take a while...')
+contact_map, distance_map, contact_map_names = ba.salt_bridge_contact_map(reference_struc, trajectory)
+print('Contact map made') 
 
-df.to_csv('sb_data.csv', index=False)
-df.to_excel('sb_data.xlsx', index=False)
+with open('contact_map.npy', 'wb') as f:
+
+    np.save(f, contact_map)
+
+with open('distance_map.npy', 'wb') as f:
+
+    np.save(f, distance_map)
+
+with open('contact_map_names.npy', 'wb') as f:
+
+    np.save(f, contact_map_names)
+
+
+
+
