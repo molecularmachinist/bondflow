cc57df05091344f21bd694272e89a65b3e37b830
micaela <micaela.roschier@helsinki.fi>
2024-11-13 12:56:54 +0200

    improved functions

--- salt_bridge_analysis/sb_bond_functions.py
+++ salt_bridge_analysis/sb_bond_functions.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import MDAnalysis as mda
 from MDAnalysis.analysis import contacts
 import pandas as pd 
@@ -9,96 +8,102 @@ def salt_bridge_contact_map(ref, trj):
 
     u = mda.Universe(ref, trj)
 
-    #Contact map
+    # Collect all positive and negative residues 
+    positive_residues = u.select_atoms("((resname ARG and name CZ) or (resname LYS and name CE))")
+    negative_residues = u.select_atoms("((resname ASP and name CG) or (resname GLU and name CD))")
 
-    #Collect all negative and positive residues
+    num_pos_residues = len(positive_residues)
+    num_neg_residues = len(negative_residues)
+    num_combinations = num_pos_residues * num_neg_residues
+    num_frames = len(u.trajectory)
 
-    positive_residues = u.select_atoms("(resname ARG) and (name CZ)") + u.select_atoms("(resname LYS) and (name CE)")
-    negative_residues = u.select_atoms("(resname ASP) and (name CG)") + u.select_atoms("(resname GLU) and (name CD)")
+    # Pre-allocate arrays
+    binary_contact_map = np.zeros((num_combinations, num_frames), dtype=np.int8)
+    contact_map_names = np.empty(num_combinations, dtype='U15')
+    distance_map = np.zeros((num_combinations, num_frames))
 
+    # Generate contact map names
+    n = 0
+    for pos_res in positive_residues:
+        for neg_res in negative_residues:
+            contact_map_names[n] = f"{pos_res.resname}{pos_res.resid}_{neg_res.resname}{neg_res.resid}"
+            n += 1
 
-    i = len(positive_residues)*len(negative_residues)
-    j = len(u.trajectory) #time
-
-    binary_contact_map = np.zeros((i,j)) # i, j = residue combination, frame
-    contact_map_names =  np.chararray((i),itemsize=15)
-    distance_map = np.zeros((i,j)) # i, j = residue combination distance, frame
-
+    # Compute distances and contact map
     for ts in u.trajectory:
-        n = 0
-        for pos_res in positive_residues:
-            
-            for neg_res in negative_residues: 
-                n += 1
-                if ts.frame == 0:
-                    
-                    contact_map_names[n-1] =  str(pos_res.resname) + str(int(pos_res.resid)) + '_' + str(neg_res.resname) + str(int(neg_res.resid))
-                
-                dist = contacts.distance_array(pos_res.position, neg_res.position)
-                
-                distance_map[n-1, int(ts.frame)] = dist
-                
-                if dist <= 6.0:
-                    
-                    binary_contact_map[n-1, int(ts.frame)] = 1
+        pos_positions = positive_residues.positions
+        neg_positions = negative_residues.positions
 
-    return binary_contact_map, distance_map, contact_map_names # Returns a contact map with only 1 and 0, a distance map where there are the distances of the chosen residues, and a map of residue pairing names
+        # Calculate distance matrix between positive and negative residues
+        dist_matrix = contacts.distance_array(pos_positions, neg_positions)
+        
+        # Flatten distance matrix and store in distance map
+        distance_map[:, ts.frame] = dist_matrix.flatten()
+
+        # Update binary contact map based on distance threshold
+        binary_contact_map[:, ts.frame] = (dist_matrix.flatten() <= 6.0).astype(np.int8)
+
+    return binary_contact_map, distance_map, contact_map_names
 
 
-def create_sorted_distance_dataframe(contact_map_names, binary_contact_map, distance_map):
     
-    std_contact_map = np.std(binary_contact_map, axis=1)
 
+def create_sorted_distance_dataframe(contact_map_names, contact_map, distance_map):
+
+
+    # Calculate standard deviation of the binary contact map
+    std_contact_map = np.std(contact_map, axis=1)
+
+    # Get indices of non-zero standard deviations
     indices = np.flatnonzero(std_contact_map)
+    stds = std_contact_map[indices]
 
-    j = indices
-    sb_names = []
-    stds = []
-    for n in range(len(j)):
-        index_i = j[n]
-        
-        sb_name = contact_map_names[index_i]
-        sb_name = sb_name.decode("UTF-8")
-       
-        sb_names.append(sb_name)
-        std = std_contact_map[index_i]
-        stds.append(std)
+    # Extract names and standard deviations
+    sb_names = contact_map_names[indices].astype(str)
 
-    data = {'Salt bridge': sb_names,
-            'Standard deviation': stds,
-        'Index': indices}
-    
-    #Create dataframe
+    # Create DataFrame and sort by standard deviation
+    data = {'Salt bridge': sb_names, 'Standard deviation': stds, 'Index': indices}
     df_std = pd.DataFrame(data)
+    df_std_sorted = df_std.sort_values(by='Standard deviation', ascending=False)
 
-    #Sort the standard deviations highest to lowest
-    df_std_sorted=df_std.sort_values(by=['Standard deviation'], ascending=False)
+    # Extract the sorted indices
+    sorted_indices = df_std_sorted['Index'].values
 
-    dist = []
-    sb_names = []
+    # Extract and transpose the relevant distances
+    dist = distance_map[sorted_indices].T
 
-    for ind in df_std_sorted.index:
-        index, name = df_std_sorted['Index'][ind], df_std_sorted['Salt bridge'][ind]
-        
-        time_dependence = distance_map[index,:]
-        dist.append(list(time_dependence))
-        sb_names.append(name)
-        
-    dist = np.array(dist)
-    dist = np.transpose(dist)
+    # Create the final DataFrame with distances
+    distance_data_frame = pd.DataFrame(dist, columns=df_std_sorted['Salt bridge'])
 
-    distance_data_frame = pd.DataFrame(dist, columns= sb_names)
-    
-    return distance_data_frame, sb_names, df_std_sorted
-
+    return distance_data_frame, df_std_sorted['Salt bridge'].tolist(), df_std_sorted
 
 
 
 
 def moving_average(data, n):
+    if n < 1:
+        raise ValueError("Window size n must be at least 1")
+    if n > len(data):
+        raise ValueError("Window size n must not be larger than the data length")
+    
+    # Compute the cumulative sum
+    cumsum = np.cumsum(np.insert(data, 0, 0)) 
+    cumsum = np.delete(cumsum, 0)
+    mvalue = (cumsum[n:] - cumsum[:-n]) / n
+
+    # Pad the start and end to match the size of the input array
+    start_padding = [np.mean(data[:i+1]) for i in range(n//2)]
+    end_padding = [np.mean(data[-(i+1):]) for i in range(n//2, 0, -1)]
+ 
+    mvalue = np.concatenate([start_padding, mvalue, end_padding])
+
+    return mvalue
+
+
+
+def slow_moving_average(data, n):
     mvalue = []
 
-    # Käydään läpi datan alkiot, joita ennen olevat alkiot ovat kaikki keskiarvoistusikkunassa:
     for k in range(0, n//2):
         data_in_window = data[:k+n//2+1]
         mvalue.append(sum(data_in_window)/len(data_in_window))
@@ -107,7 +112,6 @@ def moving_average(data, n):
         data_in_window = data[k-n//2:k+n//2+1]
         mvalue.append(sum(data_in_window)/len(data_in_window))
 
-    # Lopuksi datan alkiot, joiden jälkeen olevat alkiot ovat kaikki keskiarvoistusikkunassa:
     for k in range(len(data)-n//2, len(data)):
         data_in_window = data[k-n//2:]
         mvalue.append(sum(data_in_window)/len(data_in_window))
@@ -116,12 +120,12 @@ def moving_average(data, n):
 
 
 
-def moving_average_contact_map_maker(contact_map_names, binary_contact_map,ma_window):
+def moving_average_contact_map_maker(binary_contact_map,ma_window):
     
     ma_binary_contact_map = np.zeros(shape=(binary_contact_map.shape))
 
-    j = len(contact_map_names)
-    for n in range(j):
+    
+    for n in range(len(binary_contact_map[:,0])):
         index_i = n
             
         contacts = binary_contact_map[index_i,:]
